9dde5266dd3e31b2aff673af8e9521e2744ae3ed
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-11 18:43:09 -0300

    Continuando prj, logica de sorteio de palavras já esta feito, e componente da pagina de jogar tambem esta finalizado

--- prj-secret-word/src/App.js
+++ prj-secret-word/src/App.js
@@ -25,25 +25,56 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //pegando uma categoria aleatoria
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category)
+
+    //pegando uma palavra desta categoria aleatoria
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+    console.log(word);
+
+    return { word, category };
+  }
+
   //Início do jogo
-  const startGame = () =>{
+  const startGame = () => {
+    // escolhendo a palavra e escolhendo a categoria
+    const { word, category } = pickWordAndCategory();
+    let wordLetters = word.split('');
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    //pegando a palavra e transformando em letras
+    console.log(word, category);
+    console.log(wordLetters)
+    
+    //setando os estados
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   }
 
   //verfificando a palavra
-   const verifyLetter = () =>{
-      setGameStage(stages[2].name);
-   }
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
 
-   //reinicializar o jogo
-   const retry = () =>{
+  //reinicializar o jogo
+  const retry = () => {
     setGameStage(stages[0].name);
-   }
+  }
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
