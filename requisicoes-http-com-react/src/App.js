cd14ff194b23480788c6c4c7c0557aac5ccf2e51
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-16 19:26:43 -0300

    Utilizadno o metodo POST para enviar dados com react

--- requisicoes-http-com-react/src/App.js
+++ requisicoes-http-com-react/src/App.js
@@ -6,6 +6,8 @@ const url = "http://localhost:3000/products"
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   //resgatando dados
   useEffect(() => {
@@ -17,16 +19,42 @@ function App() {
     fetchData()
   }, []);
 
-  console.log(products);
+  // adiconando produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    })
+
+  };
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product)=>(
+        {products.map((product) => (
           <li key={product.id}>Produto: {product.name} - Preço: R$ {product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onClick={handleSubmit}>
+          <label>
+            Nome: <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+            Preço: <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Cadastrar" />
+        </form>
+      </div>
     </div>
   );
 }
