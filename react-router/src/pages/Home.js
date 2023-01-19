096a9524300219381ef59a38453ec90f169cb887
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-19 19:21:19 -0300

    Carregando os dados para poder ver melhor a troca de paginas.

--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
+
 import './Home.css'
+
 const Home = () => {
+  // carregando dados
+
+  const url = "http://localhost:3000/products"
+
+  const{data: items, loading, error} = useFetch(url)
+
+  
   return (
     <div>
-        <p>Esta é a Home</p>
+      <h2>Produtos</h2>
+      {error && <p>{error}</p>}
+      <ul className='products'>
+      {items &&
+          items.map((item) => (
+            <li key={item.id}>
+              <h3>{item.name}</h3>
+              <p>R$: {item.price}</p>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
