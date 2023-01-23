bdb54b0bb7be731d311cd6229fbf8cf058d62c22
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-23 18:21:09 -0300

    Classe de lnk ativo automatica ao importar NavLink

--- react-router/src/components/NavBar.js
+++ react-router/src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './NavBar.css'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre Nós</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre Nós</NavLink>
     </nav>
   )
 }
