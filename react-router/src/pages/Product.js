75e4acebfd85dc9f73a65fa01a98f93f51caccc4
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-23 17:48:11 -0300

    Carregameto dinamico do dados

--- react-router/src/pages/Product.js
+++ react-router/src/pages/Product.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import React from 'react';
+import './Product.css';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 const Product = () => {
 
     // rota dinamica
-    const { id } = useParams()
+    const { id } = useParams();
+
+    // carregamento do dado individual
+    const url = "http://localhost:3000/products/" + id
+    const {data: product, loading, error} = useFetch(url);
+    console.log(product)
     return (
-        <>
+        <div className='productDiv'>
         <p>Id do produto: {id}</p>
-        </>
+        {error && <p>Ocorreu um erro..</p>}
+        {loading && <p>Carregando..</p>}
+        {product && (
+            <div>
+                <h2>{product.name}</h2>
+                <h3>Preço: <b>{product.price}</b></h3>
+            </div>
+            
+        )}
+        </div>
     )
 }
 
