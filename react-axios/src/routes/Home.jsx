2a16f9905cefa3af65826c1fa1ccb5e736f7f9c6
VinyProgramador <vinicius.nunes@sptech.school>
2023-02-10 09:53:50 -0300

    Já fazendo a requisição com o axios, pegando a API JsonPlacholder, para dados do blog.

--- react-axios/src/routes/Home.jsx
+++ react-axios/src/routes/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 import './Home.css'
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const getPosts = async () => {
+    try{
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const data = response.data;
+      console.log(data)
+    }
+    catch(error){
+      console.log(error+" Erro na chamada..")
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
   return (
     <div>Home</div>
   )
